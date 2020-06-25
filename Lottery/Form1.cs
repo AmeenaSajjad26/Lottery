402e4e9ac6c457ee06c4075a8ea3332d2d052cb3
Ameena_Sajjad <ameenasajjad26@gmail.com>
2020-06-25 19:12:50 -0400

    Initial commit

---
+++ Lottery/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ameenaMid2
+{
+    public partial class Form1 : Form
+    {
+        //array of lottery random numbers
+        public int[] numbersArray = new int[6];
+
+        //object of class MyNumbers
+        MyNumbers number = new MyNumbers();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //if (e.key == Keys.Escape)
+            {
+                DialogResult result = MessageBox.Show("Are you sure?", "Exiting", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    //this.Close();
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // if (e.key == Keys.Enter)
+            {
+                numbersArray[0] = number.getRandomNumber();
+                numbersArray[1] = number.getRandomNumber();
+                numbersArray[2] = number.getRandomNumber();
+                numbersArray[3] = number.getRandomNumber();
+                numbersArray[4] = number.getRandomNumber();
+                numbersArray[5] = number.getRandomNumber();
+
+                //sort this array 
+                Array.Sort(numbersArray);
+
+                //call function to avoid duplications
+                noDuplicate();
+
+                //sort this array 
+                Array.Sort(numbersArray);
+
+                textBox2.Text = concatenation();
+            }
+        }
+
+        public void noDuplicate()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbersArray[i] == numbersArray[j])
+                        numbersArray[i] = number.getNumbers(j);//make a new one
+                }
+            }
+        }
+        public string concatenation()
+        {
+            return Convert.ToString(numbersArray[0]) + "  " + Convert.ToString(numbersArray[1]) + "  " + Convert.ToString(numbersArray[2]) + "  " +
+                Convert.ToString(numbersArray[3]) + "  " + Convert.ToString(numbersArray[4]) + "  " + Convert.ToString(numbersArray[5]);
+
+        }
+
+
+    }//end of class
+}//end of spacename
---
+++ Lottery/MyNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ameenaMid2
+{
+    public class MyNumbers
+    {
+        private Random rand;
+
+        public Random Rand
+        {
+            get
+            {
+                return rand;
+            }
+            set
+            {
+                rand = value;
+            }
+        }
+
+        //constructor
+        public MyNumbers()
+        {
+            this.Rand = new Random();
+        }
+
+
+        //get a random number from 1 to 48
+        public int getRandomNumber()
+        {
+            return Rand.Next(1, 49);
+        }
+
+        //get another random number that is different from others
+        public int getNumbers(int x)
+        {
+            int y = getRandomNumber();
+
+            while (y == x)
+            {
+                y = getRandomNumber();
+            }
+
+            return y;
+        }
+
+    }//end of class
+}//end of spacename
